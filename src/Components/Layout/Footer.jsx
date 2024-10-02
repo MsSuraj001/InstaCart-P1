eee9526400b5bf7669d9e9e54f0ccca45f2a119d
MsSuraj001 <milukumar225@gmail.com>
2024-10-02 16:03:40 +0530

    added the footer

--- src/Components/Layout/Footer.jsx
+++ src/Components/Layout/Footer.jsx
@@ -1,10 +1,23 @@
 import React from 'react'
+import IconInstagram from '../All_SVG/InstaSVG'
+import IconBxlWhatsapp from '../All_SVG/WhatsappSVG'
+import IconUpwork from '../All_SVG/UpworkSVG'
+import IconFacebook from '../All_SVG/FaceBookSVG'
 
 function Footer() {
   return (
-    <div>
-      <h1>footer</h1>
-    </div>
+    <footer className='h-[13vh] w-full flex justify-between items-center md:px-20 px-7 bg-slate-300'>
+        <div>
+            <p>Copyright 2020 some text for your choice</p>
+        </div>
+
+        <div className='flex gap-2'>
+            <a href=""><IconInstagram className="md:text-3xl text-2xl"/></a>
+            <IconBxlWhatsapp className="md:text-3xl text-2xl"/>
+            <IconFacebook className="md:text-3xl text-2xl"/>
+            <IconUpwork className="md:text-3xl text-2xl"/>
+        </div>
+    </footer>
   )
 }
 
--- src/Components/Layout/Navbar.jsx
+++ src/Components/Layout/Navbar.jsx
@@ -4,7 +4,7 @@ import IconSunMoon from '../All_SVG/MoonSVG'
 
 function Navbar() {
   return (
-    <nav className='h-[13vh] w-full bg-slate-200 flex justify-between items-center md:px-20 shadow-lg'>
+    <nav className='h-[13vh] w-full bg-slate-200 flex justify-between items-center md:px-20 px-7 shadow-lg'>
       <div>
         <h1 className='text-3xl font-bold text-blue-500'>InstaCart</h1>
       </div>
