b8e3d60c9de7494486a46b4439d891378ab37d44
MsSuraj001 <milukumar225@gmail.com>
2024-10-09 11:26:43 +0530

    build the our service with responsive

--- src/Components/Pages/Service.jsx
+++ src/Components/Pages/Service.jsx
@@ -9,8 +9,26 @@ function Service() {
         <h1>Our Service</h1>
       </div>
       {/* // this is the 1st div  */}
-      <div className='w-full flex justify-center items-center'>
+      <div className='w-full flex flex-col sm:flex-row justify-center items-center px-16 gap-6'>
 
+        <div className='w-full  md:w-1/2 lg:w-1/3 border-2 py-8 px-4 rounded-lg shadow-md'>
+          <h1 className='py-3 text-2xl font-serif text-center'>SOCIAL MEDIA MANAGMENT</h1>
+          <h1 className='py-3 text-xl font-light'>Instagram linkden and more</h1>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem, beatae nihil accusantium molestiae pariatur quisquam maxime, a, aliquid iste laboriosam veniam quasi deserunt. Fuga natus, atque maxime cupiditate voluptas recusandae.</p>
+
+          <button className='pt-2'>
+            <a href='#' className='text-blue-500 py-3'>Learn More</a>
+          </button>
+        </div>
+        <div className='w-full  md:w-1/2 lg:w-1/3 border-2 py-8 px-4 rounded-lg shadow-md'>
+          <h1 className='py-3 text-2xl font-serif text-center'>SOCIAL MEDIA MANAGMENT</h1>
+          <h1 className='py-3 text-xl font-light'>Instagram linkden and more</h1>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem, beatae nihil accusantium molestiae pariatur quisquam maxime, a, aliquid iste laboriosam veniam quasi deserunt. Fuga natus, atque maxime cupiditate voluptas recusandae.</p>
+
+          <button className='pt-2'>
+            <a href='#' className='text-blue-500 py-3'>Learn More</a>
+          </button>
+        </div>
         <div className='w-full  md:w-1/2 lg:w-1/3 border-2 py-8 px-4 rounded-lg shadow-md'>
           <h1 className='py-3 text-2xl font-serif text-center'>SOCIAL MEDIA MANAGMENT</h1>
           <h1 className='py-3 text-xl font-light'>Instagram linkden and more</h1>
