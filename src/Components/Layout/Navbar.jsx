cf8b035e20a5099c119416f9217bd462ecb97b16
MsSuraj001 <milukumar225@gmail.com>
2024-10-20 15:21:52 +0530

    added the profile pic and some code

--- src/Components/Layout/Navbar.jsx
+++ src/Components/Layout/Navbar.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import IconHome_svg from '../All_SVG/HomeSVG'
 import IconSunMoon from '../All_SVG/MoonSVG'
+import { Link } from 'react-router-dom'
 
 function Navbar() {
   return (
@@ -9,8 +10,12 @@ function Navbar() {
         <h1 className='text-3xl font-bold text-white'>InstaCart</h1>
       </div>
 
-      <div>
-        <IconSunMoon className="text-4xl text-white"/>
+      <div className='flex gap-3 justify-center items-center text-white'>
+        <Link>HOME</Link>
+        <Link>ABOUT</Link>
+        <Link>CONTACT</Link>
+        <Link>SERVICE</Link>
+        <IconSunMoon className="text-2xl text-white"/>
       </div>
     </nav>
   )
--- src/Components/Pages/Home.jsx
+++ src/Components/Pages/Home.jsx
@@ -7,13 +7,14 @@ import Layout from '../Layout/Layout'
 import Spline from '@splinetool/react-spline';
 import Portfolio from './Portfolio'
 import SErviceA from './ServiceA'
+import Pf from '../All_SVG/P-Pic_New.png'
 
 function Home() {
   return (
     <Layout>
     <div className='w-full flex flex-col md:flex-row bg-[#222831] text-white'>
         {/* this is the left section */}
-      <div className='h-full md:[h-50%] w-full md:w-[50%] px-10 py-24 md:px-24'>
+      <div className='h-full md:[h-50%] w-full md:w-[50%] px-10 md:py-24 md:px-24'>
             <div className='text-start'>
                 <span className='font-bold tracking-widest'>HELLO WORLD,  </span><span className='font-bold text-blue-600 tracking-wide'>WE ARE</span>
             </div>
@@ -25,7 +26,7 @@ function Home() {
                 <span className='font-thin text-[1.5rem]'>Content Marketing Agency</span>
             </div>
 
-            <div className='hidden md:block py-6'>
+            <div className=' md:block py-6'>
                 <p>Lorem ipsum dolor sit, amet consectetur adipisicing quas aliquid ipsum distinctio quisquam. Sunt dolorum ullam repudiandae.</p>
 
                 <div className='py-5 text-2xl flex gap-2'>
@@ -44,7 +45,9 @@ function Home() {
 
         {/* this is the right section */}
       <div>
-        <h1>this is the right section</h1>
+        {/* <h1>this is the right section</h1> */}
+        <img src={Pf} alt=""  className='h-[30rem] w-[100%] items-center mt-12 border-b-2'/>
+        
       </div>
 
     </div>
